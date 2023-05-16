074869bf1adc4d20f4429cc99986360a5db21e6d
AlperBozyel <alperbozyel07@gmail.com>
2023-05-16 14:03:12 +0300

    -m deneme

--- First Case/src/main/java/com/example/first_case/app/FirstCaseApplication.java
+++ First Case/src/main/java/com/example/first_case/app/FirstCaseApplication.java
@@ -12,7 +12,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
-
+import org.hibernate.annotation;
 
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class FirstCaseApplication {
